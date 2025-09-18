f85aa79e7e6d5fbb63c58d65c442cca892cfc2e7
racoonbot <41879013+racoonbot@users.noreply.github.com>
2025-09-18 14:12:55 +0300

    f доработали префаб пули

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     void Start()
     {
-        
+        Destroy(gameObject, 5f);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
