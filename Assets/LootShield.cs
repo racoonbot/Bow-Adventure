d1d057a2d35c2600fe377b7c90f4c6f1fc0278df
racoonbot <41879013+racoonbot@users.noreply.github.com>
2025-11-07 13:00:43 +0300

    Добавлен звук бафа щита

--- Assets/LootShield.cs
+++ Assets/LootShield.cs
@@ -4,19 +4,37 @@ using UnityEngine;
 public class LootShield : MonoBehaviour
 {
     private Shield shield;
-    
+    private AudioSource audioSource;
+    private float delay = 2;
     private void Start()
     {
         shield = FindObjectOfType<Shield>();
+        audioSource = GetComponent<AudioSource>();
+       
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<Bullet>() != null)
         {
-            shield.ActivateShield(); 
-            
-            Destroy(gameObject); 
+            shield.ActivateShield();
+            if (audioSource != null)
+            {
+                audioSource.Play();
+                Debug.Log("Звук щита воспроизводится.");
+                StartCoroutine(DestroyAfterDelay(2f)); // Уничтожаем через 2 секунды
+            }
+            else
+            {
+                Debug.LogError("AudioSource не найден.");
+            }
         }
     }
+
+    private IEnumerator DestroyAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(gameObject);
+    }
+
 }
