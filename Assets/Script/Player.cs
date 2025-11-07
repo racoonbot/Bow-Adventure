5fe86f8f4b234b70cdc6b33a63c802d44f124f26
racoonbot <41879013+racoonbot@users.noreply.github.com>
2025-11-07 14:29:16 +0300

    Настроены материалы тела с бафом и без бафа!
    Материалы взяты с лута.  Нужны свои

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,12 +4,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-   public GameObject BulletPrefab;
-   public Trajectory trajectory;
-   public bool isBuffed = false;
+    public GameObject BulletPrefab;
+    public Trajectory trajectory;
+    public bool isBuffed;
+    private GameObject _playerBody;
+    private Renderer _renderer;
+    public Material materialBuff; // Материал для состояния бафа
+    public Material defaultMaterial; // Дефолтный материал (укажите в инспекторе)
+
+    private void Start()
+    {
+        
+        _playerBody = FindObjectOfType<PlayerBody>().gameObject;
+        _renderer = _playerBody.GetComponent<MeshRenderer>();
+    }
 
     void Update()
     {
+        if (isBuffed)
+        {
+            _renderer.material = materialBuff; // Устанавливаем материал бафа
+        }
+        else
+        {
+            Debug.Log("Не баф материал");
+            _renderer.material = defaultMaterial; // Устанавливаем дефолтный материал
+        }
+        
         if (Input.GetMouseButtonUp(0) && !isBuffed)
         {
             Rigidbody rb = Instantiate(BulletPrefab, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
@@ -19,17 +40,24 @@ public class Player : MonoBehaviour
 
         if (Input.GetMouseButtonUp(0) && isBuffed)
         {
-            Rigidbody rb1 = Instantiate(BulletPrefab, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+         
+            Rigidbody rb1 = Instantiate(BulletPrefab, transform.position, Quaternion.identity)
+                .GetComponent<Rigidbody>();
             rb1.gameObject.layer = LayerMask.NameToLayer("BulletPlayer");
-            rb1.AddForce(trajectory.direction * -trajectory.force, ForceMode.Impulse); 
-            
-            Rigidbody rb2 = Instantiate(BulletPrefab, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb1.AddForce(trajectory.direction * -trajectory.force, ForceMode.Impulse);
+
+            Rigidbody rb2 = Instantiate(BulletPrefab, transform.position, Quaternion.identity)
+                .GetComponent<Rigidbody>();
             rb2.gameObject.layer = LayerMask.NameToLayer("BulletPlayer");
-            rb2.AddForce((trajectory.direction + trajectory.transform.up.normalized * -0.15f) * -trajectory.force, ForceMode.Impulse); 
-            
-            Rigidbody rb3 = Instantiate(BulletPrefab, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb2.AddForce((trajectory.direction + trajectory.transform.up.normalized * -0.15f) * -trajectory.force,
+                ForceMode.Impulse);
+
+            Rigidbody rb3 = Instantiate(BulletPrefab, transform.position, Quaternion.identity)
+                .GetComponent<Rigidbody>();
             rb3.gameObject.layer = LayerMask.NameToLayer("BulletPlayer");
-            rb3.AddForce((trajectory.direction + trajectory.transform.up.normalized * 0.15f) * -trajectory.force, ForceMode.Impulse);
+            rb3.AddForce((trajectory.direction + trajectory.transform.up.normalized * 0.15f) * -trajectory.force,
+                ForceMode.Impulse);
         }
+        
     }
 }
