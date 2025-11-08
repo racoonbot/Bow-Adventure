5a1f7bea9be58939403a80d0eb2d383f3996aa7c
racoonbot <41879013+racoonbot@users.noreply.github.com>
2025-11-08 20:17:43 +0300

    f Починил экран икончания игры

--- Assets/Script/SceneLoader.cs
+++ Assets/Script/SceneLoader.cs
@@ -7,7 +7,7 @@ public class SceneLoader : MonoBehaviour
 {
     private PlayerHealth playerHealth;
 
-    private void Start()
+    private void Awake()
     {
         playerHealth = FindObjectOfType<PlayerHealth>();
         Debug.Log(playerHealth != null ? "PlayerHealth найден." : "PlayerHealth не найден.");
@@ -33,8 +33,8 @@ public class SceneLoader : MonoBehaviour
     {
         if (playerHealth != null)
         {
-            playerHealth.OnDead += LoadGameOverScene; //Не подписывается
             Debug.Log("Подписка на событие OnDead успешно выполнена.");
+            playerHealth.OnDead += LoadGameOverScene; //Не подписывается
         }
     }
 
